751426dac5b73d7f3aaf244d31dfcfb461898dbf
hussain <hussain.kantawala@uniken.com>
2020-02-05 16:48:39 +0530

    Androidx related import changes

--- android/src/main/java/co/eleken/react_native_touch_id_android/FingerprintModule.java
+++ android/src/main/java/co/eleken/react_native_touch_id_android/FingerprintModule.java
@@ -7,7 +7,7 @@ import android.content.pm.PackageManager;
 import android.hardware.biometrics.BiometricPrompt;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Build;
-import android.support.v4.app.ActivityCompat;
+import androidx.core.app.ActivityCompat;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
